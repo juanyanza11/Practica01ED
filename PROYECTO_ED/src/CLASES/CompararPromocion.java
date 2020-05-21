4d0540d7f2a1637be84b41ebbb455ed26d23c38f
juanyanza11 <ww4fjj@gmail.com>
2020-05-20 22:34:30 -0500

    Borrando proyecto empaquetado

--- PROYECTO_ED/src/CLASES/CompararPromocion.java
+++
@@ -1,9 +0,0 @@
-package CLASES;
-
-public class CompararPromocion implements Comparable<Alumno>{
-    @Override
-    public int compareTo(Alumno a) {
-        Alumno a1 = new Alumno();
-        return a1.getPromocion().compareTo(a.getPromocion());
-    }
-}
